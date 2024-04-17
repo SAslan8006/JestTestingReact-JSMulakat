764065b35f20a4b98c85a636f3ce0d7dd3fed94b
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-04-17 09:58:24 +0300

    Başlangıç

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1: abcabcbb => abc uzunluğu 3  olduğu şeklindedir... en uzun alt dizi
+// bbbbbbbb => b => 1 ve uzunluk 1
+
+const longestSubstring = (str) => {
+    return str;
+}
+
+console.log(longestSubstring("abcabcbb")); // "abc"
